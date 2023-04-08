24d716f6162faaccff410bb9ae850946846f13f5
HorizonsMW <mw711990838@gmail.com>
2023-04-09 01:16:22 +0300

    Build 20 - stable, no news yet - bummer

---
+++ newsCopy.js
@@ -0,0 +1,20 @@
+console.log("NEWS COPY JS LOADED");
+
+function getFiles() {
+  const xmlReq = new XMLHttpRequest();
+  const path = "./docs/news/";
+  var files = [];
+  xmlReq.open("GET", path, true);
+  xmlReq.onreadystatechange = function () {
+    console.log(xmlReq.status);
+    if (xmlReq.readyState == 4 && xmlReq.status == 200) {
+      files = xmlReq.responseText.split("\n");
+      for (var i = 0; i < files.length; i++) {
+        console.log(files[i]);
+      }
+    }
+  };
+  xmlReq.send();
+}
+
+getFiles();
